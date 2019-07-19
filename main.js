9edcbf90ca505e072adb253ac4c061c3e3adae03
BadBrahmin <tejasshree95@gmail.com>
2019-07-19 14:47:24 +0530

    add createUI function

--- main.js
+++ main.js
@@ -26,6 +26,36 @@ function addTodo(event) {
     }
 }
 
+//CreateUI Function: Takes data from allTodos to create list
+
+function createUI(data = []) {
+    innerText = "";     //clears the UI when a new task is added to avoid duplication
+    data.forEach((todo, index) => {
+        //create todo in a Para
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.textContent = todoText;
+        //create checkbox
+        const input = document.createElement("input");
+        input.classList.add("list");
+        input.type = "checkbox";
+        input.addEventListener("click", isDoneFunc);
+        input.checked = todo.isDone;
+        input.setAttribute("data-id", index);
+        //Span for Delete button
+        const span = document.createElement("span");
+        span.textContent: "X";
+        span.setAttribute("data-id", index);
+        span.addEventListener("click", deleteTodo);
+
+        li.appendChild(input);
+        li.appendChild(p);
+        li.appendChild(span);
+        ul.appendChild(li);
+
+    })
+}
+
 
 
 
